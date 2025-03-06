4d4eb1ab652dc0101e56acbb72a664b9d0cf0cea
Wachirawit27 <s42885@bj.ac.th>
2025-03-06 11:11:14 +0700

    Add files via upload

---
+++ final_27_/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1741234175,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/cloud-sheet0.png",
+		"images/crosshair-sheet0.png",
+		"images/b1-sheet0.png",
+		"images/border-sheet0.png",
+		"images/blood.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}

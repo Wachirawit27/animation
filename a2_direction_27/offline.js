3088add0d5a418064c7478513e89baeed8744044
Wachirawit27 <s42885@bj.ac.th>
2025-03-06 09:06:07 +0700

    Add files via upload

---
+++ a2_direction_27/offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1741226722,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background.png",
+		"images/sprite-sheet0.png",
+		"images/collide-sheet0.png",
+		"images/collide2-sheet0.png",
+		"images/collide3-sheet0.png",
+		"images/wall.png",
+		"images/playeranimations-sheet0.png",
+		"images/playeranimations-sheet1.png",
+		"images/player-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
